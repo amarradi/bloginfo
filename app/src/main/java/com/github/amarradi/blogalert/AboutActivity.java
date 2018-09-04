f1ad8ea1d2ca933e03b78fa368894ba4c4e148a2
Marcus Radisch <radischm@gmail.com>
2018-09-04 16:39:20 +0200

    Menu-Aktionen hinzugefügt.

--- app/src/main/java/com/github/amarradi/blogalert/AboutActivity.java
+++ app/src/main/java/com/github/amarradi/blogalert/AboutActivity.java
@@ -1,26 +1,45 @@
 package com.github.amarradi.blogalert;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
-
+    @SuppressLint("StringFormatInvalid")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
         TextView appVersion = findViewById(R.id.tvVersion);
-        appVersion.setText("Version: " + BuildConfig.VERSION_NAME);
+        Resources resources = getResources();
+
+        String version_text = String.format(resources.getString(R.string.version), BuildConfig.VERSION_NAME);
+        appVersion.setText(version_text);
+        //appVersion.setText(R.string.version + BuildConfig.VERSION_NAME.toString());
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        inflater.inflate(R.menu.menu_about, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.thanks:
+                Intent intentThanks = new Intent(this, ThanksActivity.class);
+                startActivity(intentThanks);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/github/amarradi/blogalert/ThanksActivity.java
+++ app/src/main/java/com/github/amarradi/blogalert/ThanksActivity.java
@@ -1,5 +1,6 @@
 package com.github.amarradi.blogalert;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -23,10 +25,20 @@ public class ThanksActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        inflater.inflate(R.menu.menu_thanks, menu);
         return true;
     }
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                Intent intentAbout = new Intent(this, AboutActivity.class);
+                startActivity(intentAbout);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 
 }
