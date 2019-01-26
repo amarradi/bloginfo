0772dd60a817a533b6c4a8195e58754ebbbe6cd7
Stefan Richter <sealor@users.noreply.github.com>
2019-01-26 13:41:11 +0100

    FeedReader refactored

--- app/src/main/java/com/github/amarradi/bloginfo/FeedReader.java
+++ app/src/main/java/com/github/amarradi/bloginfo/FeedReader.java
@@ -1,74 +1,41 @@
 package com.github.amarradi.bloginfo;
 
-import android.util.Log;
-
-import com.github.amarradi.bloginfo.receivers.Channel;
-
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 
 public class FeedReader {
 
-	private static final String TAG = "data_content";
+	public static final String XML_TAG_LAST_BUILD_DATE = "lastBuildDate";
 
 	public String parseLastBuildDate(InputStream is) {
-		return parseXML(is);
-	}
-
-	private String parseXML(InputStream is) {
-		XmlPullParserFactory parserFactory;
 		try {
-
-			parserFactory = XmlPullParserFactory.newInstance();
-			XmlPullParser parser = parserFactory.newPullParser();
-
-			parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-			parser.setInput(is, null);
-
-			return processParsing(parser);
-
+			return parseXml(is);
 		} catch (XmlPullParserException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		throw new RuntimeException();
 	}
 
-	private String processParsing(XmlPullParser parser) throws XmlPullParserException, IOException {
-		ArrayList<Channel> channels = new ArrayList<>();
-		int eventType = parser.getEventType();
-		Channel currentChannel = null;
+	private String parseXml(InputStream is) throws XmlPullParserException, IOException {
+		XmlPullParserFactory parserFactory = XmlPullParserFactory.newInstance();
+		XmlPullParser parser = parserFactory.newPullParser();
 
-		while (eventType != XmlPullParser.END_DOCUMENT) {
-			String eltName = null;
-			switch (eventType) {
-				case XmlPullParser.START_TAG:
-					eltName = parser.getName();
+		parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+		parser.setInput(is, null);
 
-					if ("channel".equals(eltName)) {
-						currentChannel = new Channel();
-						channels.add(currentChannel);
-					} else if (currentChannel != null) {
-						if ("lastBuildDate".equals(eltName)) {
-							currentChannel.lastBuildDate = parser.nextText();
-						}
-					}
-					break;
-			}
-			eventType = parser.next();
+		while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
+			if (XML_TAG_LAST_BUILD_DATE.equals(parser.getName()))
+				return parser.nextText();
+
+			parser.next();
 		}
-		StringBuilder builder = new StringBuilder();
-		for (Channel channel : channels) {
-			builder.append(channel.lastBuildDate);
-		}
-		Log.i(TAG, builder.toString());
-		return builder.toString();
+
+		throw new RuntimeException();
 	}
 
 }
